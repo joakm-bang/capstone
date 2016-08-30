df2f1f828d3d7488cedf260306f38760c21895eb
joakm-bang <jocke.bang@gmail.com>
2016-08-30 22:06:11 +0200

    init

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.charts import TimeSeries
+from bokeh.embed import components
+import pandas as pd
+from random import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+	return redirect('/index')
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+	
+	dat = dict()
+	dat['Date'] = range(1000,1100)
+	dat['Price'] = []
+	for x in dat['Date']:
+		dat['Price'].append(random())
+	
+	data = pd.DataFrame.from_dict(dat)
+	plot = TimeSeries(data=data, legend=True, xlabel='Date', ylabel='$', title='Stock')
+	script, div = components(plot)  
+
+	return render_template('index.html', script=script, div=div)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+	app.run(port=33507)
