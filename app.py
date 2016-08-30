e38666359612a46c91e4c75df6830cd2aaaf089a
joakm-bang <jocke.bang@gmail.com>
2016-08-30 23:09:10 +0200

    chk

--- app.py
+++ app.py
@@ -17,49 +17,54 @@ app.api_key = 'YzCQfNaj_RRvSF7Kcfkm'  #quandl api key
 def main():
 	return redirect('/index')
 
-@app.route('/index')
+@app.route('/index',methods=['GET', 'POST'])
 def index():
 	
-	dat = dict()
-	dat['Date'] = range(1000,1100)
-	dat['Price'] = []
-	for x in dat['Date']:
-		dat['Price'].append(random())
+	if request.method == 'GET':
+		return render_template('index.html')
+	if request.method == 'POST':
 	
-	data = pd.DataFrame.from_dict(dat)
-	plot = TimeSeries(data=data, x='Date', y=['Price'], legend=True, xlabel='Date', ylabel='$', title='Stock')
-	script, div = components(plot) 
+		dat = dict()
+		dat['Date'] = range(1000,1100)
+		dat['Price'] = []
+		for x in dat['Date']:
+			dat['Price'].append(random())
+		
+		data = pd.DataFrame.from_dict(dat)
+		plot = TimeSeries(data=data, x='Date', y=['Price'], legend=True, xlabel='Date', ylabel='$', title='Stock')
+		script, div = components(plot) 
+		
+		ticker = 'GOOG'
+		series = ['Close', 'Open']
+		drange = ('2016-06-01', '2016-07-01')
+		
+		
+		#read from csv for now
+		url = 'https://www.quandl.com/api/v3/datasets/WIKI/{0}.csv?trim_start={1}&trim_end={2}&api_key={3}'.format(ticker, str(drange[0]), str(drange[1]),app.api_key)
+		try:
+			csvdata = urlopen(url).read()
+		except HTTPError:
+			return render_template('index.html', ticker=ticker, error='Invalid ticker')
+		data = pd.read_csv(StringIO(csvdata.decode('utf-8')), index_col=0, parse_dates=True)
+		data['Date'] = data.index
+		plot = TimeSeries(data=data[['Open', 'Close']], legend=True, xlabel='Date', ylabel='$', title=ticker)
+		script, div = components(plot)
+		
+		
+		url = 'https://www.quandl.com/api/v3/datasets/WIKI/{0}.json?trim_start={1}&trim_end={2}&api_key={3}'.format(ticker, str(drange[0]), str(drange[1]),app.api_key)
+		try:
+			jsondata = urlopen(url).read()
+		except HTTPError:
+			return render_template('index.html', ticker=ticker, error='Invalid ticker')
+		jsondata = json.loads(jsondata)
+		data = pd.DataFrame.from_dict(jsondata['dataset']['data'])	
+		data.columns = jsondata['dataset']['column_names']
+		data.index = data.Date
+		pd.to_datetime(data.index)
+		plot = TimeSeries(data=data, x='Date', y=series, legend=True, xlabel='Date', ylabel='$', title='Stock')
+		script, div = components(plot) 	
 	
-	ticker = 'GOOG'
-	drange = ('2016-06-01', '2016-07-01')
-	
-	
-	#read from csv for now
-	url = 'https://www.quandl.com/api/v3/datasets/WIKI/{0}.csv?trim_start={1}&trim_end={2}&api_key={3}'.format(ticker, str(drange[0]), str(drange[1]),app.api_key)
-	try:
-		csvdata = urlopen(url).read()
-	except HTTPError:
-		return render_template('index.html', ticker=ticker, error='Invalid ticker')
-	data = pd.read_csv(StringIO(csvdata.decode('utf-8')), index_col=0, parse_dates=True)
-	data['Date'] = data.index
-	plot = TimeSeries(data=data[['Open', 'Close']], legend=True, xlabel='Date', ylabel='$', title=ticker)
-	script, div = components(plot)
-	
-	
-	url = 'https://www.quandl.com/api/v3/datasets/WIKI/{0}.json?trim_start={1}&trim_end={2}&api_key={3}'.format(ticker, str(drange[0]), str(drange[1]),app.api_key)
-	try:
-		jsondata = urlopen(url).read()
-	except HTTPError:
-		return render_template('index.html', ticker=ticker, error='Invalid ticker')
-	jsondata = json.loads(jsondata)
-	data = pd.DataFrame.from_dict(jsondata['dataset']['data'])	
-	data.columns = jsondata['dataset']['column_names']
-	data.index = data.Date
-	pd.to_datetime(data.index)
-	plot = TimeSeries(data=data, x='Date', y=['Close', 'Open'], legend=True, xlabel='Date', ylabel='$', title='Stock')
-	script, div = components(plot) 	
-
-	return render_template('index.html', script=script, div=div)
+		return render_template('graph.html', script=script, div=div)
 
 if __name__ == '__main__':
 	app.run(port=33507)
