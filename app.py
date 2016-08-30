00555c43f163616a87e778ef793d43f98f7c3431
joakm-bang <jocke.bang@gmail.com>
2016-08-30 23:40:05 +0200

    fixed

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from bokeh.embed import components
 import bokeh
 import pandas as pd
 from random import random
+from datetime import datetime, timedelta
 
 import json
 from urllib2 import urlopen, HTTPError
@@ -23,13 +24,33 @@ def index():
 	if request.method == 'GET':
 		return render_template('index.html')
 	if request.method == 'POST':
-			
+		
+		lag = 1	
 		ticker = request.form['ticker']
 		keys = ['Close', 'Open', 'Adj. Open', 'Adj. Close', 'High', 'Low']
 		series = [s for s in keys if s in request.form.keys()]
 		if series == []:
 			series = ['Close']
-		drange = ('2016-07-01', '2016-08-01') #fix range	
+		
+		def pad(s):
+			s=str(s)
+			if len(s)==1:
+				s = '0'+s
+			return(s)
+		now = datetime.now()
+		dend = str(now.year)+'-'+pad(now.month)+'-'+pad(now.day)
+		go = True
+		f = 0
+		while go:
+			try:
+				dstart = str(now.year)+'-'+pad(now.month-1)+'-'+pad(now.day-f)
+				dmp = datetime.strptime(dstart, '%Y-%m-%d')
+				go = False
+			except:
+				f = f + 1
+		drange = (dstart, dend)
+		#drange = ('2016-07-01', '2016-08-01') #fix range	
+		
 		
 		url = 'https://www.quandl.com/api/v3/datasets/WIKI/{0}.json?trim_start={1}&trim_end={2}&api_key={3}'.format(ticker, str(drange[0]), str(drange[1]),app.api_key)
 		try:
